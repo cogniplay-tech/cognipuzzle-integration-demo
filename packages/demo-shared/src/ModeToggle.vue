ffa9309877ad1c2e1f0e86233f9b63184b6265e6
David Steiner <git@davidsteiner.dev>
2026-09-04 10:36:02 +0200

    Add a colour mode toggle to demo-shared

---
+++ packages/demo-shared/src/ModeToggle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useColorMode } from './useColorMode'
+
+const { dark, toggle } = useColorMode()
+</script>
+
+<template>
+  <button class="button" type="button" @click="toggle">
+    {{ dark ? 'Light mode' : 'Dark mode' }}
+  </button>
+</template>
